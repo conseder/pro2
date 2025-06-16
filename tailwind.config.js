9d98eb8505c8f913e63d23d6477be7c5ea4a216b
conseder <mohammad0arabyat@gmail.com>
2025-06-17 01:40:55 +0300

    Fix broken colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,10 @@ export default {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /(from|to|bg|text|border|ring|divide|outline)-(blue|emerald|amber|purple|red|indigo)-(50|100|200|400|600|800|900)/,
+      variants: ['dark'],
+    },
+  ],
 };
